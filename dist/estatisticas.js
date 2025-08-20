094c7d103d8eb4413df3c28b354055f7e979f236
renanmorg <renanmorg@hotmail.com>
2025-08-20 18:24:07 +0100

    estatísticas diárias

--- dist/estatisticas.js
+++ dist/estatisticas.js
@@ -1,6 +1,6 @@
 window.__ceti_receive({
   "ok": true,
-  "updatedAt": "2025-08-20T17:12:33.877Z",
+  "updatedAt": "2025-08-20T17:23:47.754Z",
   "categorias": {
     "melhor-filme": {
       "display": "Melhor Filme",
