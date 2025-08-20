d0d89fe2fa12616f9d07443f6f81fde5133c4c0f
renanmorg <renanmorg@hotmail.com>
2025-08-20 20:49:27 +0100

    build: estatísticas 2025-08-20T19:49:09.033Z

--- dist/estatisticas.js
+++ dist/estatisticas.js
@@ -1,6 +1,6 @@
 window.__ceti_receive({
   "ok": true,
-  "updatedAt": "2025-08-20T19:45:23.992Z",
+  "updatedAt": "2025-08-20T19:49:09.033Z",
   "categorias": {
     "melhor-filme": {
       "display": "Melhor Filme",
@@ -192,7 +192,7 @@ window.__ceti_receive({
           "historico": [
             {
               "semana": "26/01/2026",
-              "percent": 96,
+              "percent": 86,
               "posicao": 1
             },
             {
@@ -369,7 +369,7 @@ window.__ceti_receive({
           {
             "posicao": 1,
             "nome": "Tessa Thompson",
-            "percent": 96
+            "percent": 86
           },
           {
             "posicao": 2,
