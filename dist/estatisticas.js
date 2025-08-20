08c248371b11af14f4faea85f8390197191634f4
renanmorg <renanmorg@hotmail.com>
2025-08-20 17:35:00 +0100

    estatísticas diárias

---
+++ dist/estatisticas.js
@@ -0,0 +1,359 @@
+window.__ceti_receive({
+  "ok": true,
+  "updatedAt": "2025-08-20T16:34:37.148Z",
+  "categorias": {
+    "melhor-filme": {
+      "display": "Melhor Filme",
+      "url": "https://www.termometrooscar.com/melhor-filme.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-direcao": {
+      "display": "Melhor Direção",
+      "url": "https://www.termometrooscar.com/melhor-diretor.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-ator": {
+      "display": "Melhor Ator",
+      "url": "https://www.termometrooscar.com/melhor-ator.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-atriz": {
+      "display": "Melhor Atriz",
+      "url": "https://www.termometrooscar.com/melhor-atriz.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-ator-coadjuvante": {
+      "display": "Melhor Ator Coadjuvante",
+      "url": "https://www.termometrooscar.com/melhor-ator-coadjuvante.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-atriz-coadjuvante": {
+      "display": "Melhor Atriz Coadjuvante",
+      "url": "https://www.termometrooscar.com/melhor-atriz-coadjuvante.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-roteiro-original": {
+      "display": "Melhor Roteiro Original",
+      "url": "https://www.termometrooscar.com/melhor-roteiro-original.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-roteiro-adaptado": {
+      "display": "Melhor Roteiro Adaptado",
+      "url": "https://www.termometrooscar.com/melhor-roteiro-adaptado.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-filme-internacional": {
+      "display": "Melhor Filme Internacional",
+      "url": "https://www.termometrooscar.com/melhor-filme-estrangeiro.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-animacao": {
+      "display": "Melhor Animação",
+      "url": "https://www.termometrooscar.com/melhor-animaccedilatildeo.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-documentario": {
+      "display": "Melhor Documentário",
+      "url": "https://www.termometrooscar.com/melhor-documentaacuterio.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-direcao-de-arte": {
+      "display": "Melhor Direção de Arte",
+      "url": "https://www.termometrooscar.com/melhor-direccedilatildeo-de-arte.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-figurino": {
+      "display": "Melhor Figurino",
+      "url": "https://www.termometrooscar.com/melhor-figurino.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-maquiagem": {
+      "display": "Melhor Maquiagem",
+      "url": "https://www.termometrooscar.com/melhor-maquiagem.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-fotografia": {
+      "display": "Melhor Fotografia",
+      "url": "https://www.termometrooscar.com/melhor-fotografia.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-edicao": {
+      "display": "Melhor Edição",
+      "url": "https://www.termometrooscar.com/melhor-ediccedilatildeo.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-efeitos-visuais": {
+      "display": "Melhor Efeitos Visuais",
+      "url": "https://www.termometrooscar.com/melhor-efeitos-visuais.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-cancao-original": {
+      "display": "Melhor Canção Original",
+      "url": "https://www.termometrooscar.com/melhor-canccedilatildeo-original.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-trilha-sonora": {
+      "display": "Melhor Trilha Sonora",
+      "url": "https://www.termometrooscar.com/melhor-trilha-sonora.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-som": {
+      "display": "Melhor Som",
+      "url": "https://www.termometrooscar.com/melhor-ediccedilatildeo-de-som.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-curta-metragem": {
+      "display": "Melhor Curta-Metragem",
+      "url": "https://www.termometrooscar.com/melhor-curta-metragem.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-animacao-curta": {
+      "display": "Melhor Animação em Curta-Metragem",
+      "url": "https://www.termometrooscar.com/melhor-animaccedilatildeo-em-curta-metragem.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-documentario-curta": {
+      "display": "Melhor Documentário em Curta-Metragem",
+      "url": "https://www.termometrooscar.com/melhor-documentaacuterio-em-curta-metragem.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-escalacao-de-elenco": {
+      "display": "Melhor Escalação de Elenco",
+      "url": "https://www.termometrooscar.com/melhor-escalacao-de-elenco.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "melhor-coordenacao-de-dubles": {
+      "display": "Melhor Coordenação de Dublês",
+      "url": "https://www.termometrooscar.com/melhor-coordenacao-de-dubles.html",
+      "semanas": [],
+      "candidatos": [],
+      "rankingPorSemana": {}
+    },
+    "teste-novo-categoria": {
+      "display": "Teste Novo Categoria",
+      "url": "https://www.termometrooscar.com/testecategorianovo.html",
+      "semanas": [
+        "26/01/2026",
+        "23/01/2026"
+      ],
+      "candidatos": [
+        {
+          "nome": "Tessa Thompson",
+          "descricao": "Hedda",
+          "imagem": "https://www.termometrooscar.com/uploads/1/4/8/8/1488234/s118581882355374300_p135_i18_w800.jpeg",
+          "historico": [
+            {
+              "semana": "26/01/2026",
+              "percent": 42,
+              "posicao": 3
+            },
+            {
+              "semana": "23/01/2026",
+              "percent": 30.3,
+              "posicao": 1
+            }
+          ]
+        },
+        {
+          "nome": "Tarika",
+          "descricao": "",
+          "imagem": "https://www.termometrooscar.com/uploads/1/4/8/8/1488234/s118581882355374300_p583_i9_w2000.jpeg",
+          "historico": [
+            {
+              "semana": "26/01/2026",
+              "percent": 79,
+              "posicao": 1
+            },
+            {
+              "semana": "23/01/2026",
+              "percent": 23,
+              "posicao": 2
+            }
+          ]
+        },
+        {
+          "nome": "Ana Paula",
+          "descricao": "O Maio Filme de Todos os Tempos com titulo",
+          "imagem": "https://www.termometrooscar.com/uploads/1/4/8/8/1488234/s118581882355374300_p135_i18_w800.jpeg",
+          "historico": [
+            {
+              "semana": "26/01/2026",
+              "percent": 2,
+              "posicao": 5
+            },
+            {
+              "semana": "23/01/2026",
+              "percent": 10,
+              "posicao": 3
+            }
+          ]
+        },
+        {
+          "nome": "Absurdo",
+          "descricao": "Turquia",
+          "imagem": "https://www.termometrooscar.com/uploads/1/4/8/8/1488234/s118581882355374300_p135_i18_w800.jpeg",
+          "historico": [
+            {
+              "semana": "26/01/2026",
+              "percent": 44,
+              "posicao": 2
+            },
+            {
+              "semana": "23/01/2026",
+              "percent": 3,
+              "posicao": 5
+            }
+          ]
+        },
+        {
+          "nome": "Teste",
+          "descricao": "",
+          "imagem": "https://www.termometrooscar.com/uploads/1/4/8/8/1488234/s118581882355374300_p135_i18_w800.jpeg",
+          "historico": [
+            {
+              "semana": "26/01/2026",
+              "percent": 31,
+              "posicao": 4
+            },
+            {
+              "semana": "23/01/2026",
+              "percent": 4,
+              "posicao": 4
+            }
+          ]
+        },
+        {
+          "nome": "Teste agora",
+          "descricao": "agota",
+          "imagem": "https://www.termometrooscar.com/uploads/1/4/8/8/1488234/s118581882355374300_p135_i18_w800.jpeg",
+          "historico": [
+            {
+              "semana": "26/01/2026",
+              "percent": 2,
+              "posicao": 5
+            },
+            {
+              "semana": "23/01/2026",
+              "percent": 3,
+              "posicao": 5
+            }
+          ]
+        }
+      ],
+      "rankingPorSemana": {
+        "23/01/2026": [
+          {
+            "posicao": 1,
+            "nome": "Tessa Thompson",
+            "percent": 30.3
+          },
+          {
+            "posicao": 2,
+            "nome": "Tarika",
+            "percent": 23
+          },
+          {
+            "posicao": 3,
+            "nome": "Ana Paula",
+            "percent": 10
+          },
+          {
+            "posicao": 4,
+            "nome": "Teste",
+            "percent": 4
+          },
+          {
+            "posicao": 5,
+            "nome": "Absurdo",
+            "percent": 3
+          },
+          {
+            "posicao": 5,
+            "nome": "Teste agora",
+            "percent": 3
+          }
+        ],
+        "26/01/2026": [
+          {
+            "posicao": 1,
+            "nome": "Tarika",
+            "percent": 79
+          },
+          {
+            "posicao": 2,
+            "nome": "Absurdo",
+            "percent": 44
+          },
+          {
+            "posicao": 3,
+            "nome": "Tessa Thompson",
+            "percent": 42
+          },
+          {
+            "posicao": 4,
+            "nome": "Teste",
+            "percent": 31
+          },
+          {
+            "posicao": 5,
+            "nome": "Ana Paula",
+            "percent": 2
+          },
+          {
+            "posicao": 5,
+            "nome": "Teste agora",
+            "percent": 2
+          }
+        ]
+      }
+    }
+  }
+});
