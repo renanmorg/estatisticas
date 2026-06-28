d2d3efa6b93c91d6a649cc9916ed02d0d85326dd
renanmorg <renanmorg@hotmail.com>
2026-06-28 02:01:41 +0100

    build: estatísticas 2026-06-28T01:01:31.597Z

--- dist/estatisticas.js
+++ dist/estatisticas.js
@@ -1,6 +1,6 @@
 window.__ceti_receive({
   "ok": true,
-  "updatedAt": "2026-06-27T01:01:32.365Z",
+  "updatedAt": "2026-06-28T01:01:31.597Z",
   "categorias": {
     "melhor-filme": {
       "display": "Melhor Filme",
