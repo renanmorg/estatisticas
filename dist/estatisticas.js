732f37eb8be2c343c8958fb336d84043a378d3a0
renanmorg <renanmorg@hotmail.com>
2026-07-30 02:01:45 +0100

    build: estatísticas 2026-07-30T01:01:32.563Z

--- dist/estatisticas.js
+++ dist/estatisticas.js
@@ -1,6 +1,6 @@
 window.__ceti_receive({
   "ok": true,
-  "updatedAt": "2026-07-29T01:01:33.029Z",
+  "updatedAt": "2026-07-30T01:01:32.563Z",
   "categorias": {
     "melhor-filme": {
       "display": "Melhor Filme",
