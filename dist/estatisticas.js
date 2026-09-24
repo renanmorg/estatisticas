594122d2859a41f90d61962fe297694764062457
renanmorg <renanmorg@hotmail.com>
2026-09-24 02:01:44 +0100

    build: estatísticas 2026-09-24T01:01:31.767Z

--- dist/estatisticas.js
+++ dist/estatisticas.js
@@ -1,6 +1,6 @@
 window.__ceti_receive({
   "ok": true,
-  "updatedAt": "2026-09-23T01:01:32.747Z",
+  "updatedAt": "2026-09-24T01:01:31.767Z",
   "categorias": {
     "melhor-filme": {
       "display": "Melhor Filme",
